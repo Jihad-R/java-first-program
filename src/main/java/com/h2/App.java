123a95e8e861025befd01083a01677e396b66579
Jihad-R <jihad.jobo@gmail.com>
2023-04-03 16:53:10 -0500

    full program

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,7 +4,7 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
@@ -12,16 +12,14 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return 2*number;
+        return 2 * number;
     }
-    
+
     private static int add(int[] numbers) {
-    	int sum = 0;
-    	
-    	for(int x = 0; x<numbers.length;x++) {
-    		sum = sum + numbers[x];
-    	}
-    	
-    	return sum;
+        var sum = 0;
+        for(int number: numbers) {
+            sum += number;
+        }
+        return sum;
     }
 }
